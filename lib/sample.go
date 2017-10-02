4e78af10ea0b952e25070ee15dc0d46f313d46b1
shibayu36 <shibayu36@gmail.com>
2017-10-02 18:40:52 +0900

    Implements sample plugin

---
+++ lib/sample.go
@@ -0,0 +1,58 @@
+package mpsample
+
+import (
+	"flag"
+	"math/rand"
+	"strings"
+	"time"
+
+	mp "github.com/mackerelio/go-mackerel-plugin"
+)
+
+// SamplePlugin mackerel plugin
+type SamplePlugin struct {
+	Prefix string
+}
+
+// MetricKeyPrefix interface for PluginWithPrefix
+func (p *SamplePlugin) MetricKeyPrefix() string {
+	if p.Prefix == "" {
+		p.Prefix = "sample"
+	}
+	return p.Prefix
+}
+
+// GraphDefinition interface for mackerelplugin
+func (p *SamplePlugin) GraphDefinition() map[string]mp.Graphs {
+	labelPrefix := strings.Title(p.Prefix)
+	return map[string]mp.Graphs{
+		"dice": {
+			Label: labelPrefix + " Dice Value",
+			Unit:  "integer",
+			Metrics: []mp.Metrics{
+				{Name: "d6", Label: "Dice(d6)"},
+				{Name: "d20", Label: "Dice(d20)"},
+			},
+		},
+	}
+}
+
+// FetchMetrics interface for mackerelplugin
+func (p *SamplePlugin) FetchMetrics() (map[string]float64, error) {
+	rand.Seed(time.Now().UnixNano())
+	metrics := map[string]float64{
+		"d6":  float64(rand.Intn(6) + 1),
+		"d20": float64(rand.Intn(20) + 1),
+	}
+	return metrics, nil
+}
+
+// Do the plugin
+func Do() {
+	optPrefix := flag.String("metric-key-prefix", "", "Metric key prefix")
+	flag.Parse()
+	plugin := mp.NewMackerelPlugin(&SamplePlugin{
+		Prefix: *optPrefix,
+	})
+	plugin.Run()
+}
